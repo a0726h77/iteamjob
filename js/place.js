d0fafe85157af3ca4e7707cd9dac03206b57f929
a0726h77@gmail.com <a0726h77@gmail.com@b79f7d3c-d5c6-ace9-54de-e4983a652a21>
2011-06-23 07:22:31 +0000

    修改、增加地點搜尋

---
+++ js/place.js
@@ -0,0 +1,33 @@
+﻿    var country = new Array();    // 地區
+    country[0] = '請選擇地點';
+    country[1] = '台北';
+    country[2] = '台中';
+    country[3] = '高雄';
+
+ 
+    var city = new Array();
+    city[1] = new Array();    // 台北
+    city[1][0] = new Array();
+    city[1][0][0] = '請選擇景點';
+    city[1][0][1] = '';
+    city[1][1] = new Array();
+    city[1][1][0] = '淡水老街';
+    city[1][1][1] = '25.172767,121.440897';
+    city[1][2] = new Array();
+    city[1][2][0] = '士林夜市';
+    city[1][2][1] = '25.084328,121.52501000000007';
+    city[1][3] = new Array();
+    city[1][3][0] = '101';
+    city[1][3][1] = '25.034322,121.564858';
+    city[1][4] = new Array();
+    city[1][4][0] = '師大夜市';
+    city[1][4][1] = '25.02659,121.52992199999994';
+
+    city[2] = new Array();    // 台中
+    city[2][0] = '台中火車站';
+    city[2][1] = '美術館';
+    city[2][2] = '圓形劇場';
+ 
+    city[3] = new Array();    // 高雄
+    city[3][0] = '夢時代';
+    city[3][1] = '六和夜市';
